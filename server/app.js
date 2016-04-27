51314aa8814e7e1098d1478c1fbe465aa528e09b
Fuzu Li <lifuzu@gmail.com>
2016-04-27 10:04:50 -0700

    Enable account management with passport

--- server/app.js
+++ server/app.js
@@ -9,6 +9,11 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config.json');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy
+var flash    = require('connect-flash');
+var session  = require('express-session');
+
 mongoose.connect(config.mongodsn);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongo connection error:'));
@@ -35,6 +40,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+// required for passport
+app.use(session({ secret: 'ilovesamplejsonjsonsample', resave: false, saveUninitialized: false })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -51,6 +62,12 @@ app.use('/api/v1/quotes', api_quotes);
 app.use('/api/v1/likes', api_likes);
 app.use('/api/v1/shares', api_shares)
 app.use('/api/v1/samples', api_samples)
+
+// passport config
+var Account = require('./app/models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport')
+var Account = require('../app/models/account')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var json;
-  if (req.query.json) {
-    json = JSON.parse(req.query.json);
-  }
+  // var json;
+  // if (req.query.json) {
+  //   json = JSON.parse(req.query.json);
+  // }
   // TOOD: Parse meta tag to get more metadata from the link
   // if (json.link) {
   //   var meta = metatag(json.link);
   //   json = merge(json, meta);
   // }
 
-  console.log(json);
-  res.render('index', { json: json });
-});
+  // console.log(json);
+  res.render('index', { user: req.user });
+})
+
+router.get('/register', function(req, res) {
+  res.render('register', { });
+})
+
+router.post('/register', function(req, res) {
+  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+    if (err) {
+      return res.render('register', { error : err.message });
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      req.session.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/');
+      });
+    });
+  });
+})
+
+router.get('/login', function(req, res) {
+  res.render('login', { user : req.user });
+})
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/')
+})
+
+router.get('/logout', function(req, res) {
+  req.logout()
+  res.redirect('/')
+})
+
+router.get('/ping', function(req, res){
+    res.status(200).send("pong!")
+})
 
 module.exports = router;
